3d32d8a41afb8584727cd99fbae85c855c26646a
wjddbals <jumin0220@naver.com>
2023-11-23 12:59:00 +0900

    Initial commit

---
+++ src/test/java/com/example/mapoboard/MapoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapoboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
